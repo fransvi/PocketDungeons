9cd1e65b60a4826134903c85fb4599dd9fc02690
Frank <xelimra@gmail.com>
2017-04-06 12:34:39 +0300

    PlayerInventory

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject[] _mWeapons;
+    [SerializeField]
+    private GameObject[] _sWeapons;
+    [SerializeField]
+    private GameObject _activeMainWeapon;
+
+
+    private bool _hasKey1;
+    private float _playerGold;
+    private bool _hasHealthPotion;
+    private GameObject _activeOffWeapon;
+
+	// Use this for initialization
+	void Start () {
+        _playerGold = 0f;
+        _hasHealthPotion = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
