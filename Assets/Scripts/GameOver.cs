49ada22176f8a63423d2f3c62b6d8a8e5587e5a1
Valtteri24 <valtteriirettlav@gmail.com>
2017-04-08 20:07:49 +0300

    Game Over

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,19 +3,24 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
+	public GameObject[] buttons;
+
     private string sceneBuildIndex;
     private Scene scene;
 
     // Use this for initialization
     void Start () {
-         scene = SceneManager.GetActiveScene();
+        scene = SceneManager.GetActiveScene();
         Debug.Log("Active scene is '" + scene.name + "'.");
 	}
-	
 
-    public void load()
-    { Application.LoadLevel(scene.name);
+	void Update(){
+		
+	}
+
+    public void load() {
+		Application.LoadLevel(scene.name);
         Debug.Log("testi");
-            }
+    }
 	
 }
