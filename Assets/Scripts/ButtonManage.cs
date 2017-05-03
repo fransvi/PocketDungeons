de5cd1d6c561835257ecf9749ce23a91b1b8301e
Frank <xelimra@gmail.com>
2017-05-03 17:05:37 +0300

    Merge toimimaan

--- Assets/Scripts/ButtonManage.cs
+++ Assets/Scripts/ButtonManage.cs
@@ -55,8 +55,8 @@ public class ButtonManage : MonoBehaviour
     public void SetGameManager(GameManager g)
     {
         gm = g;
-        buttons[5].onClick.AddListener(gm.ResetSaveData);
-        buttons[6].onClick.AddListener(gm.SaveData);
+        //buttons[5].onClick.AddListener(gm.ResetSaveData);
+        //buttons[6].onClick.AddListener(gm.SaveData);
     }
 
     public void RetryBtn()
