359776a64adeb3685d10aa80c30ed2e63f146379
Frank <xelimra@gmail.com>
2017-05-10 13:06:58 +0300

    Bigchest ja movingplat

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -14,9 +14,13 @@ public class MovingPlatformScript : MonoBehaviour {
     public float _moveTime;
     private bool _movingDown;
     private bool _movingUp;
+    private GameObject _player;
+    private bool _playerOnPlatform;
 
 	// Use this for initialization
 	void Start () {
+        _playerOnPlatform = false;
+        _player = GameObject.FindWithTag("Player");
         if(_moveAxis == 0)
         {
             _moveDir1 = Vector3.up;
@@ -30,6 +34,46 @@ public class MovingPlatformScript : MonoBehaviour {
         _movingDown = false;
         _movingUp = false;
 
+    }
+
+    void Update()
+    {
+        Collider2D[] playerCheck = Physics2D.OverlapCircleAll(transform.position, 1f, 1 << LayerMask.NameToLayer("Default"));
+        for (int i = 0; i < playerCheck.Length; i++)
+        {
+            if (playerCheck[i].gameObject != gameObject)
+            {
+                
+
+            }
+
+        }
+        if(playerCheck.Length == 0)
+        {
+            _playerOnPlatform = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _playerOnPlatform = true;
+
+        }
+    }
+
+    void OnCollisionExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _playerOnPlatform = false;
+        }
+    }
+
+    public void SetPlayerOnPlatform(bool b)
+    {
+        _playerOnPlatform = b;
     }
 	
 	// Update is called once per frame
@@ -45,10 +89,18 @@ public class MovingPlatformScript : MonoBehaviour {
         if (_movingDown)
         {
             transform.position += _moveDir2 * _moveMultiplr;
+            if (_moveAxis == 1 && _playerOnPlatform)
+            {
+                _player.transform.position += _moveDir2 * _moveMultiplr;
+            }
         }
         else if (_movingUp)
         {
             transform.position += _moveDir1 * _moveMultiplr;
+            if(_moveAxis == 1 && _playerOnPlatform)
+            {
+                _player.transform.position += _moveDir1 * _moveMultiplr;
+            }
         }
 	}
     IEnumerator PlatTimerDown()
